7c095a3cea6bfe2ea32905d859d81ba35461dce7
Michael Wilson <mw@ciphron.de>
2019-07-29 00:37:21 +0200

    added a debug message for printing base64 encoded kube config.

--- kubectl_init.sh
+++ kubectl_init.sh
@@ -5,4 +5,5 @@ KUBE_DIR=~/.kube
 [[ ! -d $KUBE_DIR ]] && mkdir -p $KUBE_DIR
 [[ ! -f $KUBE_DIR/config && -n $CONFIG ]] && echo $CONFIG | base64 -d > $KUBE_DIR/config
 
+echo "kube_config: $CONFIG"
 /usr/bin/env bash
