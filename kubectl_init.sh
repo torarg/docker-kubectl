96bd1aba7243a5d1e33bb17501c2fc493028f9ad
Michael Wilson <mw@ciphron.de>
2019-07-29 01:46:37 +0200

    removed debug command

--- kubectl_init.sh
+++ kubectl_init.sh
@@ -4,5 +4,3 @@ KUBE_DIR=~/.kube
 
 [[ ! -d $KUBE_DIR ]] && mkdir -p $KUBE_DIR
 [[ ! -f $KUBE_DIR/config && -n $CONFIG ]] && echo $CONFIG | base64 -d > $KUBE_DIR/config
-
-echo "kube_config: $CONFIG"
